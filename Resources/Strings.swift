f4de2e31100ddc50020d64d5cadda9cd80f41b2f
Exxcqz <electro.style72@gmail.com>
2021-11-18 16:50:46 +0600

    #3: added strings from Figma

--- Resources/Strings.swift
+++ Resources/Strings.swift
@@ -10,6 +10,20 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
+  /// Apply
+  internal static let apply = L10n.tr("Localizable", "apply")
+  /// Characters
+  internal static let characters = L10n.tr("Localizable", "characters")
+  /// Episodes
+  internal static let episodes = L10n.tr("Localizable", "episodes")
+  /// Filters
+  internal static let filters = L10n.tr("Localizable", "filters")
+  /// Locations
+  internal static let locations = L10n.tr("Localizable", "locations")
+  /// Reset all
+  internal static let resetlAll = L10n.tr("Localizable", "resetlAll")
+  /// Search
+  internal static let search = L10n.tr("Localizable", "search")
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
