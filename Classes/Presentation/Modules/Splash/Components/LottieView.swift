189f7af28b64d820ba87acdd8ba15a2fc9044c4f
Alexander Loshakov <alevas556@icloud.com>
2021-11-22 14:33:52 +0300

    #3: Splash-Screen added

---
+++ Classes/Presentation/Modules/Splash/Components/LottieView.swift
@@ -0,0 +1,31 @@
+//
+//  Created by Александр Васильевич on 22.11.2021
+//  Copyright © 2021 Ronas IT. All rights reserved.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .loop
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView()
+        let animationView = AnimationView()
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.animationSpeed = 1.0
+        animationView.play()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {}
+}
--- Classes/Presentation/Modules/Splash/Components/SplashHelloComponent.swift
+++ Classes/Presentation/Modules/Splash/Components/SplashHelloComponent.swift
@@ -7,8 +7,27 @@ import SwiftUI
 
 struct SplashHelloComponent: View {
 
+    let heightScreen = UIScreen.main.bounds.size.height
+    
     var body: some View {
-        Text("Hello, Splash 👋")
+        ZStack {
+            Color(Asset.Colors.blackBG.color).edgesIgnoringSafeArea(.all)
+            Image(Asset.SplashImages.splashStars.name)
+                .resizable(resizingMode: .stretch)
+                .padding()
+                .padding(.top, 98.0)
+                .edgesIgnoringSafeArea(.top)
+            VStack {
+                Image(Asset.SplashImages.splashTitles.name)
+                    .padding(.top, 60.0)
+                LottieView(name: "portal", loopMode: .loop)
+                    .scaledToFit()
+                    .padding(.horizontal)
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.top, 90.0)
+        }
     }
 }
 
