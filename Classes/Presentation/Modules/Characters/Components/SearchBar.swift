585da6b4f6b42f3baa8be74acc778d5998222d7a
Alexander Loshakov <alevas556@icloud.com>
2021-12-01 23:42:10 +0300

    #5: update searchBar with a new design

--- Classes/Presentation/Modules/Characters/Components/SearchBar.swift
+++ Classes/Presentation/Modules/Characters/Components/SearchBar.swift
@@ -38,14 +38,12 @@ struct SearchBar: View {
             )
                 .placeholder(when: searchText.isEmpty) {
                     HStack {
-                        if !isSearching && !isEditing {
-                            Image(Asset.Icons.icSearchSmall.name)
-                                .renderingMode(.template)
-                                .resizable()
-                                .scaledToFit()
-                                .foregroundColor(Color(Asset.Colors.grayDark.name))
-                                .frame(width: Layout.scaleFactorW * 20, height: Layout.scaleFactorW * 20)
-                        }
+                        Image(Asset.Icons.icSearchSmall.name)
+                            .renderingMode(.template)
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(Color(Asset.Colors.grayDark.name))
+                            .frame(width: Layout.scaleFactorW * 20, height: Layout.scaleFactorW * 20)
                         Text(L10n.Placeholder.search)
                             .font(Font.appFontMedium(ofSize: Layout.scaleFactorW * 16))
                             .kerning(-0.32)
@@ -54,42 +52,12 @@ struct SearchBar: View {
                             .scaledToFill()
                     }
                 }
-                .padding(.vertical, Layout.scaleFactorW * 16)
-                .padding(.horizontal, Layout.scaleFactorW * 52)
+                .frame(height: Layout.scaleFactorW * 52)
+                .padding(.horizontal, Layout.scaleFactorW * 24)
                 .background(Color(Asset.Colors.blackCard.name))
                 .foregroundColor(Color(Asset.Colors.grayDark.name))
+                .accentColor(Color(Asset.Colors.primary.name))
                 .cornerRadius(16)
-                .overlay(
-                    HStack {
-                        if isEditing {
-                            Image(Asset.Icons.icSearchSmall.name)
-                                .renderingMode(.template)
-                                .foregroundColor(Color(Asset.Colors.grayDark.name))
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.leading, Layout.scaleFactorW * 16)
-                            Button(
-                                action: {
-                                    withAnimation {
-                                        self.searchText = ""
-                                        self.isEditing = false
-                                        self.isSearching = false
-                                        UIApplication.shared.dismissKeyboard()
-                                    }
-                                },
-                                label: {
-                                    Image(systemName: "multiply.circle.fill")
-                                        .foregroundColor(Color(Asset.Colors.grayDark.name))
-                                        .padding(.trailing, Layout.scaleFactorW * 16)
-                                }
-                            )
-                        }
-                    }
-                )
-                .onTapGesture {
-                    withAnimation {
-                        self.isEditing = true
-                    }
-                }
         }
     }
 }
